6b26a3c4ae1d133cd166f389da244cc0faa61c05
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2020-11-07 22:00:47 -0600

    submitting latest updates

---
+++ quiz.js
@@ -0,0 +1,161 @@
+//questions
+  var timer;
+  var timerInterval;
+  var display;
+  var questionText = document.querySelector('questionText');
+  var answerText = document.querySelector('answerText');
+  var ul = document.querySelector('#choices');
+  var currentQuestionIndex = 0;
+
+  var startButton = document
+  .querySelector('#startButton')
+  .addEventListener('click', startButtonClicked);
+
+
+
+
+  var allQuestions = [
+    {
+      prompt: "What is Ferdinads favorite color?",
+      choices: ["Red", "Blue","Green","Carolina Blue"],
+      answer: "Green"
+    },
+    {
+      prompt: "What is Ferdinads favorite sport?",
+      choices: ["Basketball", "Football","Tennis","Soccer"],
+      answer: "Football"
+    },
+    {
+        prompt: "What year was Ferdinad born",
+      choices: ["1879", "1998","1992","yesterday"],
+      answer: "1992"
+    },
+    {
+        prompt: "How tall is Ferdinad Aguwa",
+      choices: ["7'5", "6'1","5'5","6'0"],
+      answer: "6'0"
+    },
+  ]
+
+//timer
+function startTimer(duration, display) {
+    timer = duration;
+    var minutes;
+    var seconds;
+    timerInterval = setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+  
+      display.textContent = minutes + ':' + seconds;
+  
+      if (--timer < 0) {
+        // timer = duration;
+      }
+    }, 1000)
+  }
+
+
+
+//start the time of event
+  function startButtonClicked(i) {
+    var fiveMinutes = 60,
+    display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+    displayQuestion();
+    
+  }
+
+  function answerButtonClicked(i) {
+  //checks to see if button is correct
+  //if not correct deduct by 15 seconds
+  if (this.value !== allQuestions[currentQuestionIndex].answer) {
+    alert('Try again');
+    timer -= 10;
+
+  } else {
+    alert('You are Correct');
+  }
+
+  currentQuestionIndex++;
+  displayQuestion();
+}
+
+
+  function endGame() {
+    clearInterval(timerInterval);
+    console.log('game over');
+    //display results div
+    document.querySelector('#resultsDIV').style.display = 'block';
+    //hide choices div
+    document.querySelector('.container').style.display = 'none';
+    //add score final
+    document.querySelector('#score').textContent = timer;
+    document.querySelector('#time').textContent = timer;
+  }
+
+//
+  function displayQuestion(i) {
+    var currentQuestion = allQuestions[currentQuestionIndex];
+    if (!currentQuestion) {
+      return endGame();
+    }
+  //changing question title from the one in html.. into the title of the array
+  document.querySelector('#questionText').textContent = currentQuestion.prompt;
+  document.querySelector('#answerText').innerHTML = '';
+  ul.innerHTML = '';
+
+//this is for when you click on button of answer choice to have a button
+for (var i = 0; i< currentQuestion.choices.length; i++) {
+  var li = document.createElement('li');
+  var choiceButton = document.createElement('button');
+  choiceButton = document.createElement('button');
+  choiceButton.setAttribute('value', currentQuestion.choices[i]);
+  choiceButton.textContent = currentQuestion.choices[i];
+  choiceButton.onclick = answerButtonClicked;
+  choiceButton.classList.add('answerChoice');
+  li.append(choiceButton);
+  ul.append(li);
+
+  }
+}
+
+var time = document.getElementById('timer');
+time.addEventListener('click, startButtonClicked');
+
+
+
+document.querySelector('#submit').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var initials = document.querySelector('#initials').value;
+  localStorage.setItem(initials, timer);
+  displayscores();
+
+});
+
+
+document.querySelector('#resultsDiv').style.display = 'none';
+document.querySelector('#scoresDiv').style.display = 'none';
+
+document.querySelector('#seeHighScore').addEventListener('click, displayScores');
+function displayScores() {
+  document.querySelector('#scoresDiv').style.display = 'block';
+  Object.keys(localStorage).forEach(function (key) {
+    console.log(localStorage.getItem(key));
+  
+
+var li = document.createElement('li');
+li.textContent = key + '-' + localStorage.getItem(key);
+document.querySelector('#scoreslist').appendChild(li);
+  });
+
+  document.querySelector('.container').style.display = 'none';
+  document.querySelector('#resultsDIV').style.display = 'none';
+}
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,70 +1,13 @@
 // firt page/commands variable elements
-var startQuiz = document.getElementById("startButton");
+var startQuizBtn = document.getElementById("startButton");
 var infoDiv = document.getElementById("callDisplayButton");
 var quitQuiz = document.getElementById("quitAssessment");
-var keepQuiz = document.getElementById("continueQuiz");
-var timeEl = document.getElementById("timerBox")
 var displayQuestion= document.getElementById("hide");
-var secondsLeft = 61;
-
-// questions & choices variable elements
-var questions = document.querySelector("#quizQuestion");
-var choices = document.querySelectorAll(".choiceText");
-var progressText = document.querySelector("progressText")
-var scoreText = document.querySelector("#score")
-
-// variables to make sure my questions work
-
-var currentQuestion ={}
-var acceptingAnswers =true
-var score = 0
-var questionCounter = 0
-var availableQuestions = []
-
-// Array of objects Questions
-let questions =[
-  {
-    questions: 'What year was Ferinad Aguwa born?',
-    choice1: '1978',
-    choice1: '1997',
-    choice1: '1994',
-    choice1: '1992',
-    answer: '1992',
-  },
-  {
-    questions: 'How many siblings does Ferdinad Aguwa have?',
-    choice1: '19',
-    choice1: '1',
-    choice1: '11',
-    choice1: '12',
-    answer: '11',
-  },
-  {
-    questions: 'What year was Ferinad Aguwa born?',
-    choice1: '1978',
-    choice1: '1997',
-    choice1: '1994',
-    choice1: '1992',
-    answer: '1992',
-  },
-  {
-    questions: 'What year was Ferinad Aguwa born?',
-    choice1: '1978',
-    choice1: '1997',
-    choice1: '1994',
-    choice1: '1992',
-    answer: '1992',
-  }
-]
-
-var MAX_QUESTIONS = 4
-
- 
 
 // This is the function that makes my information box appear when the start button is clicked
 function displayInfoBox(){
     infoDiv.setAttribute("style", "display: block");
-    startQuiz.setAttribute("style", "display: none");
+    startQuizBtn.setAttribute("style", "display: none");
   
     
     console.log("isWorking?");
@@ -77,43 +20,10 @@ function firstQuestion(){
 infoDiv.setAttribute("style", "display: none");
 displayQuestion.setAttribute("style", "display: block");
 document.body.setAttribute("style","background-color:#c9e2d8e0");
-setTime()
 };
 
 // Created a function that will start my quiz
-startQuiz = ()=> {
-  questionCounter=0
-  score=0
-  availableQuestions = [...questions]
-  getNewQuestion()
-}
-
-getNewQuestion = () =>{
-  if (availableQuestions )
-}
-
-
-
-// setting my timer
-
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-    }
-
-  }, 1000);
-}
-
-
-
-
-
 
 // All event Listeners
 
-startQuiz.addEventListener("click", displayInfoBox)
-keepQuiz.addEventListener("click", firstQuestion,)
+startQuizBtn.addEventListener("click", displayInfoBox)
